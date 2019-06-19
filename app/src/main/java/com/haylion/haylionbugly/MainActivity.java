e8df6235e009e5ef03373892cfc97647726f8493
wangjianming <wangjianming@maascharge.com>
2019-06-19 15:03:29 +0800

    1.sdk升级

--- app/src/main/java/com/haylion/haylionbugly/MainActivity.java
+++ app/src/main/java/com/haylion/haylionbugly/MainActivity.java
@@ -1,5 +1,6 @@
 package com.haylion.haylionbugly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,18 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvText = (TextView) findViewById(R.id.tv_text);
-        tvVisonName = (TextView) findViewById(R.id.tv_vison);
+        tvText = findViewById(R.id.tv_text);
+        tvVisonName = findViewById(R.id.tv_vison);
         tvVisonName.setText(BuildConfig.VERSION_NAME);
         //tvText.setText("春眠不觉晓,这个有bug");//热修复前
         tvText.setText("春眠不觉晓，处处闻啼鸟");//热修复后
         tvText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //制造异常
-//                ArrayList<String> arrayList = new ArrayList();
-//                tvText.setText(arrayList.get(5));
-               Toast.makeText(getApplicationContext(), "已經修復", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
             }
         });
     }
